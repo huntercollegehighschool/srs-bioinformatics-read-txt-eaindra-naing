5d6f645595ea91be174a7c1d9c99b78e26e87c45
Eaindra Naing <79598728+eaindra-naing@users.noreply.github.com>
2022-01-14 17:32:27 +0000

    taking notes

--- Asampleread.py
+++ Asampleread.py
@@ -1,3 +1,9 @@
 """
 Write a program (doesn't have to be a function, but can be) that reads what's in sample.txt and prints it to the console.
 """
+
+fstream = open ("sample.txt", "r") 
+
+content = fstream.read ()
+print (content)
+fstream.close ()
--- Baminoacids.py
+++ Baminoacids.py
@@ -1,8 +1,23 @@
 """
 Define a function weightedstring that takes a string input protein. The function returns the weighted string of that protein based on masstable.txt.
 
-weightedstring should read from masstable.txt. It's helpful to have those masses in a dictionary.
+weightedstring (sum) should read from masstable.txt. It's helpful to have those masses in a dictionary.
 """
 
 def weightedstring(protein):
-  pass
+  fstream = open ("masstable.txt", "r") 
+  content = fstream.read () .split()
+  weightedstring = []
+  for letter in content:
+    protein = 
+  print (content)
+  fstream.close ()
+  
+weights = []
+total = 0
+file = open ("masstable.txt", "r")
+for weight in file.readlines():
+  weights [weight[0]] = float (weight[2:1])
+for p in protein:
+  total += weights [p]
+return total
